3981af2aebfea8ed083865661a25c811a4a33f7b
Maksym Ivasiv <maks1404@gmail.com>
2021-03-09 20:01:05 +0200

    first commit

---
+++ dispatcher.h
@@ -0,0 +1,45 @@
+/*
+	dispatcher.h - header file for the Dispatcher class.
+	
+	Revision 0
+	
+	Notes:
+			- 
+			
+	2016/11/19, Maya Posch
+	(c) Nyanko.ws.
+*/
+
+
+#pragma once
+#ifndef DISPATCHER_H
+#define DISPATCHER_H
+
+#include "abstract_request.h"
+#include "worker.h"
+
+#include <queue>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+
+class Dispatcher {
+	static queue<shared_ptr<AbstractRequest>> requests;
+	static queue<shared_ptr<Worker>> workers;
+	static mutex requestsMutex;
+	static mutex workersMutex;
+	static vector<shared_ptr<Worker>> allWorkers;
+	static vector<shared_ptr<thread>> threads;
+	
+public:
+	static bool init(int workers);
+	static bool stop();
+	static void addRequest(const shared_ptr<AbstractRequest>& request);
+	static bool addWorker(const shared_ptr<Worker>& worker);
+};
+
+#endif
---
+++ worker.h
@@ -0,0 +1,43 @@
+/*
+	worker.h - Header file for the Worker class.
+	
+	Revision 0
+	
+	Notes:
+			- 
+			
+	2016/11/19, Maya Posch
+	(c) Nyanko.ws
+*/
+
+
+#pragma once
+#ifndef WORKER_H
+#define WORKER_H
+
+#include "abstract_request.h"
+
+#include <condition_variable>
+#include <mutex>
+#include <memory>
+using namespace std;
+
+
+class Worker : public enable_shared_from_this<Worker>{
+	condition_variable cv;
+	mutex mtx;
+	unique_lock<mutex> ulock;
+	shared_ptr<AbstractRequest> request;
+	bool running;
+	bool ready;
+	
+public:
+
+	Worker() { running = true; ready = false; ulock = unique_lock<mutex>(mtx); }
+	void run();
+	void stop() { running = false; }
+	void setRequest(const shared_ptr<AbstractRequest>& request) { this->request = request; ready = true; }
+	void getCondition(condition_variable* &cv);
+};
+
+#endif
